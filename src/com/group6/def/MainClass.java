3483dec4f9d1e5a797a1924c65d0e9b587c6504a
Choi.SY <sollly0829@gmail.com>
2021-08-12 14:08:02 +0900

    group6 MainClass 생성

---
+++ src/com/group6/def/MainClass.java
@@ -0,0 +1,39 @@
+package com.group6.def;
+
+import java.util.Scanner;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int choice;
+		
+		System.out.println("--- 컴퓨터 ---");
+		while(true) {
+			System.out.println("1.모니터");	//최소영
+			System.out.println("2.본체");		//홍준모
+			System.out.println("3.스피커");	//정선환
+			System.out.println("4.마우스");	//주희
+			System.out.println("5.키보드");	//키보드
+			System.out.println("6.닫기");
+			System.out.print(">>>> : ");
+			choice = sc.nextInt();
+			switch(choice) {
+			case 1 :
+				System.out.println("냉장고");
+				break;	
+			case 2 :
+				break;	
+			case 3 :
+				break;
+			case 4 :
+				break;
+			case 5 :
+				break;
+			case 6 :
+				return;
+			}
+		}
+	}
+}
